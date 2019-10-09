60c03ab1af5dcc301c87b97e571878f039b05522
RicK~Z <46547196+RickillerZ@users.noreply.github.com>
2019-10-09 12:00:04 -0400

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -14,7 +14,7 @@ static const int MAX_PASSPHRASE_SIZE = 1024;
 /* BitcoinGUI -- Size of icons in status bar */
 static const int STATUSBAR_ICONSIZE = 16;
 
-static const bool DEFAULT_SPLASHSCREEN = true;
+static const bool DEFAULT_SPLASHSCREEN = false;
 
 /* Invalid field background style */
 #define STYLE_INVALID "background:#FF8080"
