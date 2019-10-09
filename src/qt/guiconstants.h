3a2872ce5da9a71f22f3f9faff3858351cdee4a5
RicK~Z <46547196+RickillerZ@users.noreply.github.com>
2019-10-09 11:57:06 -0400

    Update Website

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -47,7 +47,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "Lightbit"
-#define QAPP_ORG_DOMAIN "lightbit.org"
+#define QAPP_ORG_DOMAIN "lightbit.xyz"
 #define QAPP_APP_NAME_DEFAULT "Lightbit-Qt"
 #define QAPP_APP_NAME_TESTNET "Lightbit-Qt-testnet"
 
